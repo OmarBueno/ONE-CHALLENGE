e23ba7f6033e563dfdb02db8ada0701ff4785d4e
OmarBueno <omarzaldivar07@aragon.unam.mx>
2023-05-25 18:43:30 -0600

    version visual mejorada

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const buttonEncriptar = document.getElementById("buttonEncriptar");
 const buttonDesencriptar = document.getElementById("buttonDesencriptar");
+const buttonCopy = document.getElementById("buttonCopy");
 const inputText = document.getElementById("inputText");
 const outputText = document.getElementById("outputText");
 
@@ -15,7 +16,7 @@ function cifrar() {
     text = inputText.value;
     text = text.toLowerCase();
     hasInvalidCharacters = /[^a-z\s\n]/.test(text);
-    if (!hasInvalidCharacters) {
+    if (!hasInvalidCharacters&&text.length>0) {
     let characters = text.split("");
     let result = "";
     for (let i = 0; i < characters.length; i++) {
@@ -23,9 +24,12 @@ function cifrar() {
         result += keys[element] || element;
     }
     outputText.value = result;
+    buttonCopy.style.display = "block";
     console.log(result);}
     else {  
-        alert("No se permiten números ni caracteres especiales");
+        outputText.value = "No se permiten números ni caracteres especiales Y al menos debe tener una letra";
+        buttonCopy.style.display = "none";
+        alert("No se permiten números ni caracteres especiales Y al menos debe tener una letra");
     }
 }
 
@@ -33,20 +37,29 @@ function descifrar() {
     text = inputText.value;
     text = text.toLowerCase();
     hasInvalidCharacters = /[^a-z\s\n]/.test(text);
-    if(!hasInvalidCharacters){
+    if(!hasInvalidCharacters&&text.length>0){
     Object.keys(keys).forEach((key) => {
         text = text.replaceAll(keys[key], key);
     });
     outputText.value = text;
+    buttonCopy.style.display = "block";
     console.log(text);}
     else {
+        outputText.value = "No se permiten números ni caracteres especiales Y al menos debe tener una letra";
+        buttonCopy.style.display = "none";
         alert("No se permiten números ni caracteres especiales");
     }
+}
+
+function copy() {
+    navigator.clipboard.writeText(outputText.value);
+    console.log("Copiado");
 }
 
 function init() {
     buttonEncriptar.addEventListener("click", cifrar);
     buttonDesencriptar.addEventListener("click", descifrar);
+    buttonCopy.addEventListener("click", copy);
 }
 
 init();
